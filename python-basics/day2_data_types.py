f8f39402a04d92f2c3371c493970bb6096e539a1
VennaFutures <prudencevenna@gmail.com>
2026-01-07 19:59:04 +0300

    Day 2: Variables and data types

---
+++ python-basics/day2_data_types.py
@@ -0,0 +1,26 @@
+# Basic data types
+age = 24                     #int
+height = 1.75                #float
+name = "Venna"               #string
+is_student = True            #boolean
+monthly_study_hours = 75     #int or float
+expected_salary = 100,000.0  #float (KES)
+
+print("Age:",age)
+print("Height:",height)
+print("Name:",name)
+print("Student",is_student)
+print("Monthly Study hours:", monthly_study_hours)
+print("Expected salary (KES):", expected_salary)
+
+#Check data types
+print(type(age))
+print(type(height))
+print(type(name))
+print(type(is_student))
+print(type(monthly_study_hours))
+print(type(expected_salary))
+
+#Data Engineering journey sentence
+
+print(name, "studies", monthly_study_hours, "hours per month to become a Data Engineer.")
